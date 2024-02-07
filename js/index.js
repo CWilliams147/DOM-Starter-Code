1e9dc78a2748a9599d97095b4b335ab3262938a2
Cameron Williams <cswilliams112004@gmail.com>
2024-02-07 14:58:03 -0500

    updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 'use strict';
+console.log("boom")
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM Ready');
+
+    const todoForm = document.querySelector('#todo')
+
+    todoForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const taskInput = this.querySelector('input[name="taskEntry"]');
+        generateList(taskInput);
+    });
+
+    function generateList(taskInput) {
+        // Get List Items
+        const taskList = document.querySelector('#taskList');
+        // Generate a new LI element
+        const taskElement = document.createElement('li');
+        // Put the form value in the li element
+        taskElement.textContent = taskInput.value;
+        taskList.appendChild(taskElement);
+        clearInput(taskInput);
+    }
+
+    function clearInput (input) {
+        input.value = '';
+        return;
+    }
+});
